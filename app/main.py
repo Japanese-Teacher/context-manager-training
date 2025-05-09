df5acfaedd522f42469484c753d7d9b7808920d5
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-05-09 17:35:14 +0300

    feat: created context manager by class

--- app/main.py
+++ app/main.py
@@ -9,3 +9,17 @@ def simple_context():
 
 with simple_context():
     print("Внезапный деп")
+
+
+class MyContextManager:
+    def __enter__(self):
+        print("Вход в контекст")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Выход из контекста")
+        return False
+
+
+with MyContextManager() as cm:
+    print("Внезапный деп")
