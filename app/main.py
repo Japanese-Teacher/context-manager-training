0d308d3ea3218005d490bce3c5b5bb99215a3cdd
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-05-10 17:22:54 +0300

    style: changed the style of quotation mark

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from contextlib import contextmanager
 
 @contextmanager
-def simple_context():
-    print("Вход в контекст")
+def simple_context() -> None:
+    print('Вход в контекст')
     yield
-    print("Выход из контекста")
+    print('Выход из контекста')
 
 
 with simple_context():
-    print("Внезапный деп")
+    print('Внезапный деп')
