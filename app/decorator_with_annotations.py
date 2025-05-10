7c3024063d89c884f2b5d776766ff1aad7975b98
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-05-10 21:10:15 +0300

    feat: added [**P and R] to outer function

--- app/decorator_with_annotations.py
+++ app/decorator_with_annotations.py
@@ -5,7 +5,7 @@ from typing_extensions import ParamSpec, TypeVar
 P = ParamSpec('P')
 R = TypeVar('R')
 
-def outer_function(func: Callable[P,R]) -> Callable[P,R]:
+def outer_function[**P, R](func: Callable[P,R]) -> Callable[P,R]:
     def wrapper (*args: P.args, **kwargs: P.kwargs) -> R:
         print(f'Вызываем функцию {func.__name__}')
         print(f'Аргументы: позиционные {args}, именованные {kwargs}')
